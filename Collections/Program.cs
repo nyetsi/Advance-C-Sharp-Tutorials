7be6c988cf088ab6eb99fdd4fe88a106225d1eef
Vusumuzi Magayiyana <vusumuzi.magayiyana@kaleidocode.co.za>
2015-02-05 08:25:34 +0200

    Made changes

---
+++ Collections/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary
+            Dictionary<string, string> valuesToAdd = new Dictionary<string, string>
+            {
+                {"Name","Vusi"},
+                {"Age","25"}
+            };
+
+
+            foreach(KeyValuePair<string, string> key in valuesToAdd){
+                Console.WriteLine("{0}: {1} ", key.Key, key.Value);
+            }
+            Console.ReadKey();
+        }
+    }
+}
